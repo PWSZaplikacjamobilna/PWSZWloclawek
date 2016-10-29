347710ae64f401dcebecef8c48806929bd9bf5bf
Bartosz Moradewicz <moradewicz@hotmail.com>
2016-10-29 13:59:53 +0200

    Dodanie icony, Dodanie Logowania

--- app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/MainActivity.java
+++ app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.wloclawek.pwsz.pwszwocawek;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -97,7 +98,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_planbudynkow) {
 
         }
