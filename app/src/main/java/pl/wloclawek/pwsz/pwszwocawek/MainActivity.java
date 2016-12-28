dd0fc04dfec2181f2052348bb9c98c110b02c7db
EmilK <emisco09@gmail.com>
2016-12-28 17:37:42 +0100

    czerwony przycisk na glownej - przekierowanie na plan zajec

--- app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/MainActivity.java
+++ app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/MainActivity.java
@@ -98,8 +98,9 @@ ImageView myImgView;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), LessonPlanActivity.class);
+                view.getContext().startActivity(intent);
+
             }
         });
 
