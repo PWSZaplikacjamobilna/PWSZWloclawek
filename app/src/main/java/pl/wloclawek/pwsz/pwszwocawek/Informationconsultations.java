6e2a0f49829ce7c6e24393f6200781ba693e0806
Bartosz Moradewicz <moradewicz@hotmail.com>
2017-01-06 17:40:16 +0100

    - dodanie danych do  zakłaki "Inforamcje"

--- app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/Informationconsultations.java
+++ app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/Informationconsultations.java
@@ -1,28 +1,17 @@
 package pl.wloclawek.pwsz.pwszwocawek;
 
-import android.app.Activity;
-import android.content.DialogInterface;
-
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import android.widget.ImageView;
-
-import java.util.ArrayList;
-
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 
 public class Informationconsultations extends Fragment  {
@@ -36,6 +25,79 @@ public class Informationconsultations extends Fragment  {
 
 
 
+        Button b1 = (Button) rootView.findViewById(R.id.button1k);
+        Button b2 = (Button) rootView.findViewById(R.id.button2k);
+        Button b3 = (Button) rootView.findViewById(R.id.button3k);
+        Button b4 = (Button) rootView.findViewById(R.id.button4k);
+        Button b5 = (Button) rootView.findViewById(R.id.button5k);
+        Button b6 = (Button) rootView.findViewById(R.id.button6k);
+        Button b7 = (Button) rootView.findViewById(R.id.button7k);
+
+        b1.setText("Dyżury Zakładu Fiilologii Angielskiej, Pedagogiki oraz Studium Języków Obcych ");
+        b2.setText("Dyżury pracowników dydaktycznych Zakładu Administracji w semestrze zimowym 2016/2017");
+        b3.setText("yżury pracowników dydaktycznych Zakładu Zarządzania  ");
+        b4.setText("Dyżury pracowników dydaktycznych Zakładu Informatyki");
+        b5.setText("Dyżury pracowników dydaktycznych Zakładu Mechaniki i budowy maszyn");
+        b6.setText("Dyżury pracowników dydaktycznych Instytutu Nauk o Zdrowiu");
+        b7.setText("Dyżury pracowników Studium Wychowania Fizycznego i Sportu");
+
+       b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/Dy%C5%BCury_semestr_zima_16-17_17_listop.pdf"));
+                startActivity(browserIntent);
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/Wykaz_dyzurow_sem._zimowy_16-17.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/Wykaz_dy%C5%BCur%C3%B3w_zz_w_sem._zim._2016-17.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/Wykaz_dy%C5%BCur%C3%B3w_Inf._w_sem._zim._2016-17.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/Wykaz_dy%C5%BCur%C3%B3w_sem._zimowy_16-17_MiBM.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/student/dyzury_2016-17_zima.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pwsz.wloclawek.pl/images/student/Dy%C5%BCury_semestr_2016-17_zima.pdf"));
+                startActivity(browserIntent);
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/Informationsession.java
+++ app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/Informationsession.java
@@ -16,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import android.widget.ImageView;
@@ -35,7 +36,10 @@ public class Informationsession extends Fragment  {
         View rootView = inflater.inflate(R.layout.activity_informationsession, container, false);
 
 
-
+        String data="<!DOCTYPE html><html><body style=\"background-color: #eeeeee;\"><div class=\"webreaderArticle162\"> <p style=\"margin-bottom: 0.0001pt;\">Zimowa sesja egzaminacyjna w roku akademickim 2016/2017 trwa:&nbsp;od 01.02.2017 -&nbsp; do 14.02.2017<br>Zimowa sesja poprawkowa w roku akademickim 2016/2017 trwa:&nbsp;od 15.02.2017 - do 21.02.2017</p> <p style=\"margin-bottom: 0.0001pt;\">Termin wpisów do informatycznego systemu zarządzania procesem dydaktycznym upływa 28.02.2017.<br>Studenci III i IV roku mają obowiązek złożenia uzupełnionego indeksu wraz z kartą okresowych osiągnięć do sekretariatu właściwego Instytutu najpóźniej<strong>&nbsp;do 28.02.2017.</strong></p> <p style=\"margin-bottom: 0.0001pt;\"><strong>Terminarze zimowej sesji egzaminacyjnej w roku akademickim&nbsp;2016/2017</strong></p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej&nbsp; FILOLOGIA&nbsp; ANGIELSKA</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej PEDAGOGIKA</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej Zarządzanie - studia stacjonarne</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej Inżynieria zarządzania - studia stacjonarne</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej Informatyka&nbsp;- studia stacjonarne</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej PIELĘGNIARSTWO - studia stacjonarne</p> <p style=\"margin-bottom: 0.0001pt;\">Terminarz zimowej sesji egzaminacyjnej Administracja</p> <p style=\"margin-bottom: 0.0001pt;\">&nbsp;Terminarz zimowej sesji egzaminacyjnej Mechanika i budowa maszyn</p> <div style=\"vertical-align:top; \"> <div class=\"easyplus\"> <div id=\"fb-root\" class=\" fb_reset\"> <div style=\"position: absolute; top: -10000px; height: 0px; width: 0px;\"> <div><iframe name=\"fb_xdm_frame_https\" frameborder=\"0\" allowtransparency=\"true\" allowfullscreen=\"true\" scrolling=\"no\" id=\"fb_xdm_frame_https\" aria-hidden=\"true\" title=\"Facebook Cross Domain Communication Frame\" tabindex=\"-1\" src=\"https://staticxx.facebook.com/connect/xd_arbiter/r/iPrOY23SGAp.js?version=42#channel=f1c83ec2efeb32&amp;origin=https%3A%2F%2Fwww.pwsz.wloclawek.pl\" style=\"border: none;\"></iframe></div> </div> <div style=\"position: absolute; top: -10000px; height: 0px; width: 0px;\"> <div></div> </div> </div> </div> <div style=\"clear: both;\"></div> </div></div></body></html>";
+        WebView web = (WebView)  rootView.findViewById(R.id.WebOrg) ;
+        web.getSettings().setJavaScriptEnabled(true);
+           web.loadDataWithBaseURL("", data, "text/html", "UTF-8", "");
 
         return rootView;
     }
