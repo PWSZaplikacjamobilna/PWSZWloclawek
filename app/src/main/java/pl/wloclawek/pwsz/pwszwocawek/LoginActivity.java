e1025e310b474c23f345549796e1622a78621ac8
Bartosz Moradewicz <moradewicz@hotmail.com>
2017-01-12 17:32:00 +0100

    - minor changes

--- app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/LoginActivity.java
+++ app/src/main/java/pl/wloclawek/pwsz/pwszwocawek/LoginActivity.java
@@ -334,10 +334,7 @@ public class LoginActivity extends AppCompatActivity  {
 
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
+
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
